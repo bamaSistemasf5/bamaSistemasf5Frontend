9d8c4aaf03946fb05fb5afb6b8ea7c94eefebfae
Abelardo Acosta Cracco <102688833+Moriarty369@users.noreply.github.com>
2024-03-08 09:49:02 +0100

    Adding header template

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,46 @@
-import React from 'react'
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 export default function Footer() {
   return (
-    <div>Footer</div>
-  )
+    <footer
+      style={{
+        backgroundColor: "#232D35",
+        color: "#fff",
+        padding: "20px",
+        marginBottom: "0px",
+      }}
+    >
+      <Container>
+        <Row
+          style={{
+            display: "flex",
+            justifyContent: "space-around",
+            alignItems: "center",
+          }}
+        >
+          <Col style={{ width: "40vw", display: "flex", flexDirection: "row" }}>
+            <div className="logo-container" style={{
+            display: "flex",
+            justifyContent: "space-between",
+            alignItems: "center",
+            
+          }} >
+           
+              <img
+                style={{ maxHeight: "60px", marginRight: "1.1rem"}}
+                src="https://bama.es/wp-content/uploads/2023/05/logo-semi-white.png"
+              />
+              <span style={{ fontSize: "18px" }}>&copy; COPYRIGHT 2024</span>
+            </div>
+          </Col>
+          <Col style={{ width: "40vw", textAlign: "right" }}>
+            <span>Aviso Legal | Política de Privacidad</span>
+          </Col>
+        </Row>
+      </Container>
+    </footer>
+  );
 }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,36 @@
-import React from 'react'
+import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 
 export default function Header() {
   return (
-    <div>Header</div>
-  )
+    
+      <div  style={{ height: '30vh', width: '100v', display: 'flex', flexDirection: 'column', margin:'0' }}>
+        <div className='header-row' style={{ height: '15%', width: '100vw', backgroundColor: "#142264", color: 'white' }}>
+          {/* Aquí puedes agregar el contenido del encabezado */}
+        </div>
+        <Navbar expand="lg" className="border-bottom border-3" style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', padding: '20px' }}>
+          <Navbar.Brand href="#">
+            <img
+              src="https://bama.es/wp-content/uploads/2023/05/logo.png"
+              width="140"
+              height="80"
+              className="d-inline-block align-top"
+              alt="Logo"
+            />
+          </Navbar.Brand>
+
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+
+<Navbar.Collapse id="responsive-navbar-nav">
+  <Nav style={{ flex: 1, marginLeft: '20px' }}>
+    <Nav.Link href="#" style={{ width: '50px', display: 'flex', justifyContent: 'flex-end' }}>Inicio</Nav.Link>
+    {/* Agrega más enlaces según sea necesario */}
+  </Nav>
+</Navbar.Collapse>
+</Navbar>
+      </div>
+    
+  );
 }
