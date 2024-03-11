3e9102c5cdc7145165bcd8468255289fc2f5e513
amanda1686 <amanda160986@gmail.com>
2024-03-11 10:57:34 +0100

    Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,26 @@
 import Header from './components/Header/Header'
 import Footer from './components/Footer/Footer'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
+import Login from "./pages/Login/Login.jsx"
+import  Dashboard  from './pages/Dashboard/Dashboard.jsx';
 
 function App() {
 
 
   return (
     <>
+      <BrowserRouter>
       <Header />
-      <div>body</div>
+      <Routes>
+      <Route path='/' element={<Login />}/>
+      <Route path='/Dashboard' element={<Dashboard />} />
+      </Routes>
       <Footer />
+      </BrowserRouter>
+
     </>
   )
 }
