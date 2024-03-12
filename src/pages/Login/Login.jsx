4fbd4748c0c69aa4a5b3da481408514552c77303
amanda1686 <amanda160986@gmail.com>
2024-03-12 09:26:44 +0100

    create login

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,7 +1,46 @@
-import React from 'react'
+import React, { useState } from 'react';
+// import 'bootstrap/dist/css/bootstrap.min.css'; // Importa los estilos de Bootstrap
+import './Login.css';
 
-export default function Login() {
+const Login = ({ onLogin }) => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    onLogin({ username, password });
+    setUsername('');
+    setPassword('');
+  };
+
   return (
-    <div>Login</div>
-  )
-}
+    <>
+    <p className='acceso'>Acceso a la intranet</p>
+    <form onSubmit={handleLogin}>
+      <div className="form-group">
+        <label>Nombre de usuario:</label>
+        <input
+          type="text"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          className="form-control"
+        />
+      </div>
+      <div className="form-group">
+        <label>Contraseña:</label>
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          className="form-control"
+        />
+      </div>
+      <button type="submit" className="btn btn-iniciar">Iniciar sesión</button>
+    </form>
+    </>
+  );
+};
+
+export default Login;
+
+
