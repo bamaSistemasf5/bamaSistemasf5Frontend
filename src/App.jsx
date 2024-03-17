905b93a247f901afbb9dd8dab677a4200725b46f
Abelardo Acosta Cracco <102688833+Moriarty369@users.noreply.github.com>
2024-03-17 02:31:09 +0100

    Fixing responsive template and adding speech recognition dependencie

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes} from "react-router-dom";
 import LoadingComponent from "./components/Loading/Loading"; // Importa tu componente de carga personalizado
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
@@ -12,12 +12,13 @@ import CreateOrder from "./pages/CreateOrder/CreateOrder.jsx";
 import Invoices from "./pages/Invoices/Invoices.jsx";
 import CreateInvoice from "./pages/CreateInvoice/CreateInvoice.jsx";
 import UpdateClient from "./pages/UpdateClient/UpdateClient.jsx";
+import DeliveryNotes from "./pages/DeliveryNotes/DeliveryNotes.jsx";
 
 function App() {
   const [authenticated, setAuthenticated] = useState(false);
   const [isLoading, setIsLoading] = useState(true);
   const [hasError, setHasError] = useState(false);
-
+    // -_- -------- COMPONENTE LOADING (RENDERIZAR COMO CATCH DE ERRORES O ESPERA) --------- -_- //
   useEffect(() => {
     // Simulación de carga de datos
     const loadData = async () => {
@@ -42,35 +43,38 @@ function App() {
   const handleLogout = () => {
     setAuthenticated(false);
   };
-
-  // Condicional para mostrar el componente de carga
   if (isLoading) {
     return <LoadingComponent text="Cargando..." />;
   }
-
-  // Condicional para mostrar un mensaje de error si ha ocurrido un error
   if (hasError) {
     return <div>Error al cargar la página.</div>;
   }
 
-  // Si no hay errores ni carga en progreso, renderiza el contenido normal de la aplicación
+
+ 
   return (
     <BrowserRouter>
       <Header authenticated={authenticated} onLogout={handleLogout} />
       <Routes>
-        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Login />} />
         <Route path="/dashboard" element={<Dashboard />} />
-        {/* VISTA PARA EL USUARIO Q NO ES ADMIN VA AQUI */}
         <Route path="/clients-view" element={<ClientsView />} />
         <Route path="/create-client/" element={<CreateClient />} />
         <Route path="/update-client/:id?" element={<UpdateClient />} />
+        <Route path="/delivery-notes" element={<DeliveryNotes />} />
         <Route path="/orders-view" element={<OrdersView />} />
         <Route path="/create-order" element={<CreateOrder />} />
         <Route path="/invoices-view" element={<Invoices />} />
         <Route path="/create-invoice" element={<CreateInvoice />} />
       </Routes>
       <Footer />
+      <>
+        <p id="transcript">Transcript: {transcript}</p>
+
+        <button onClick={SpeechRecognition.startListening}>Start</button>
+      </>
     </BrowserRouter>
+    
   );
 }
 
